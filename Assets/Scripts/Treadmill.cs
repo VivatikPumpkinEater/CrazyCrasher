94bada59eb28b2e59b3f4392e44b35c19bcf7fac
VivatikPumpkinEater <vivatik.pumpkin.eater@gmail.com>
2022-07-13 17:42:32 +0300

    Added Treadmill

---
+++ Assets/Scripts/Treadmill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treadmill : MonoBehaviour
+{
+    [SerializeField] private float _power = 100f;
+    [SerializeField] private List<Transform> _miniRotators = new List<Transform>();
+
+    private void Update()
+    {
+        foreach (var miniRotator in _miniRotators)
+        {
+            miniRotator.Rotate(new Vector3(0, 0, _power * Time.deltaTime));
+        }
+    }
+}
